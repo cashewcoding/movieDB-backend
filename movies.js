dbf4e340ec3803b2cf96992866755deca1b2520d
Wiebke <wiebke.feldt@gmail.com>
2023-03-03 15:26:49 +0100

    added endpoints to get and post comments

--- movies.js
+++ movies.js
@@ -14,7 +14,7 @@ moviesRouter.get("/", (req, res) => {
     .query("SELECT * FROM movies;")
     .then((data) => {
       console.log(data);
-      res.json(data.rows);
+      res.status(200).json(data.rows);
     })
     .catch((e) => res.status(500).json({ message: e.message }));
 });
@@ -58,6 +58,24 @@ moviesRouter.get("/:id", (req, res) => {
     .catch((e) => res.status(500).json({ message: e.message }));
 });
 
+//GET COMMENTS BELONGING TO SPECIFIC MOVIE, FROM MOST RECENT TO OLDEST
+moviesRouter.get("/:id/comments", (req, res) => {
+  const movie_id = req.params.id;
+  console.log(movie_id);
+
+  pool
+    .query("SELECT * FROM comments WHERE movie_id=$1 ORDER BY created DESC;", [
+      movie_id,
+    ])
+    .then((data) => {
+      if (data.rowCount === 0) {
+        res.status(404).json({ message: "Movie has no comments" });
+      }
+      res.status(200).json(data.rows);
+    })
+    .catch((e) => res.status(500).json({ message: e.message }));
+});
+
 // CREATE NEW MOVIE
 moviesRouter.post("/", (req, res) => {
   const { title, director, year, genre, rating, poster, description, imgurl } =
@@ -70,6 +88,24 @@ moviesRouter.post("/", (req, res) => {
     .then((data) => {
       console.log(data);
       res.status(201).json(data.rows[0]);
+    })
+    .catch((e) => res.status(500).json({ message: e.message }));
+});
+
+//CREATE COMMENT TO A SPECIFIC MOVIE
+moviesRouter.post("/:id/comments", (req, res) => {
+  const movie_id = req.params.id;
+  const { author, comment } = req.body;
+  pool
+    .query(
+      "INSERT INTO comments (author, comment, created, movie_id) VALUES ($1, $2, Now(), $3) RETURNING *;",
+      [author, comment, movie_id]
+    )
+    .then((data) => {
+      if (movie_id === undefined) {
+        res.status(404).json({ message: "Movie doesn't exist." });
+      }
+      res.status(201).json(data.rows);
     })
     .catch((e) => res.status(500).json({ message: e.message }));
 });
